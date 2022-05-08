f5b38fa8d1ee4962edb18a5d08de9f6da33a7171
null <lamprecht.patrick1@gmail.com>
2022-05-08 19:10:56 +0000

    Update all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.4"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.jpa") version "1.6.10"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.example"
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     implementation("com.auth0:java-jwt:3.19.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.6")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
