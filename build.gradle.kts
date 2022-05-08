0166b466797d4d413f7f2d186864ca10417f525f
Ch4s3r <lamprecht.patrick1@gmail.com>
2022-05-08 20:18:08 +0200

    Move spring cloud to platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,8 @@ repositories {
     mavenCentral()
 }
 
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    }
-}
-
 dependencies {
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
