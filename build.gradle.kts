d5c11081d386c567256a2355580aaa6ad09e1b18
Ch4s3r <Ch4s3r@users.noreply.github.com>
2022-05-12 21:53:04 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,11 @@ repositories {
 //    }
 //}
 
+val x = "2020.0.1"
+
 
 dependencies {
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.1"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$x"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
