d3e27c3f44c6344b10a21c35d604578b1c52a960
Ch4s3r <Ch4s3r@users.noreply.github.com>
2022-06-01 17:06:48 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,8 @@ repositories {
     mavenCentral()
 }
 
-// dependencyManagement {
-//    imports {
-//        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.1")
-//    }
-// }
-
-extra["springCloudVersion"] = "2021.0.1"
-
 dependencies {
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.1"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
