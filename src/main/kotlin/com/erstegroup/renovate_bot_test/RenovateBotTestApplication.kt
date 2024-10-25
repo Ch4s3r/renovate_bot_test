a14454da4b2c80e0a2df3e10c334e9f94994eb42
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-10-25 11:02:20 +0200

    init spring boot

---
+++ src/main/kotlin/com/erstegroup/renovate_bot_test/RenovateBotTestApplication.kt
@@ -0,0 +1,11 @@
+package com.erstegroup.renovate_bot_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RenovateBotTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RenovateBotTestApplication>(*args)
+}
